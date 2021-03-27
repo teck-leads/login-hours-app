53543fc8671ac90d6135aed2bbe020aa6697a3d3
madhav <anupoju.anand@gmail.com>
2021-03-27 22:28:08 +0530

    Added CRUD operations using JdbcTemplate

--- src/main/java/com/techleads/app/dao/LoginDetailsDAO.java
+++ src/main/java/com/techleads/app/dao/LoginDetailsDAO.java
@@ -2,11 +2,11 @@ package com.techleads.app.dao;
 
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.Iterator;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Repository;
 
 import com.techleads.app.DBQueries;
@@ -14,7 +14,7 @@ import com.techleads.app.model.LoginDetails;
 
 @Repository
 public class LoginDetailsDAO {
-	
+
 	private JdbcTemplate jdbcTemplate;
 
 	@Autowired
@@ -25,85 +25,59 @@ public class LoginDetailsDAO {
 	List<LoginDetails> logins = new ArrayList<>();
 
 	public List<LoginDetails> findAll() {
-		List<LoginDetails> loginDtls = this.jdbcTemplate.query(
-				DBQueries.SELECT_ALL_LOG_DTLS,
-		        (resultSet, rowNum) -> {
-		        	LoginDetails log = new LoginDetails();
-		        	log.setId(resultSet.getInt("ID"));
-		        	log.setName(resultSet.getString("NAME"));
-		        	log.setLoginDate(resultSet.getDate("LOGIN_DTE"));
-		        	log.setActualShift(resultSet.getString("ACTUAL_SHIFT"));
-		        	log.setLoginTime(resultSet.getString("LOGIN_TIME"));
-		        	log.setLogOffTime(resultSet.getString("LOGOFF_TIME"));
-		        	log.setComments(resultSet.getString("COMMENTS"));
-		            return log;
-		        });
+		List<LoginDetails> loginDtls = this.jdbcTemplate.query(DBQueries.SELECT_ALL_LOG_DTLS, (resultSet, rowNum) -> {
+			LoginDetails log = new LoginDetails();
+			log.setId(resultSet.getInt("ID"));
+			log.setName(resultSet.getString("NAME"));
+			log.setLoginDate(resultSet.getDate("LOGIN_DTE"));
+			log.setActualShift(resultSet.getString("ACTUAL_SHIFT"));
+			log.setLoginTime(resultSet.getString("LOGIN_TIME"));
+			log.setLogOffTime(resultSet.getString("LOGOFF_TIME"));
+			log.setComments(resultSet.getString("COMMENTS"));
+			return log;
+		});
 		return loginDtls;
 	}
 
 	public String save(LoginDetails login) {
-		Object[] params= {
-				login.getId(),
-				login.getName(), 
-				login.getActualShift(),
-				new Date(),
-				login.getLoginTime(),
-				login.getLogOffTime(), 
-				login.getComments()
-				};
+		Object[] params = { login.getId(), login.getName(), login.getActualShift(), new Date(), login.getLoginTime(),
+				login.getLogOffTime(), login.getComments() };
 		int count = this.jdbcTemplate.update(DBQueries.INSERT_LOG_DTLS, params);
-		return count>0?"Login Details saved successfully with ":"Not inserted";
+		return count > 0 ? "Login Details saved successfully with " : "Not inserted";
 
 	}
 
 	public LoginDetails findById(Integer id) {
 
-		LoginDetails loginDtls = jdbcTemplate.queryForObject(
-		        DBQueries.SELECT_LOG_DTLS_BY_ID,
-		        (resultSet, rowNum) -> {
-		        	LoginDetails log = new LoginDetails();
-		        	log.setId(resultSet.getInt("ID"));
-		        	log.setName(resultSet.getString("NAME"));
-		        	log.setLoginDate(resultSet.getDate("LOGIN_DTE"));
-		        	log.setActualShift(resultSet.getString("ACTUAL_SHIFT"));
-		        	log.setLoginTime(resultSet.getString("LOGIN_TIME"));
-		        	log.setLogOffTime(resultSet.getString("LOGOFF_TIME"));
-		        	log.setComments(resultSet.getString("COMMENTS"));
-		            return log;
-		        },
-		        new Object[] {id});
-		return loginDtls;
+		SqlRowSet rowSet = jdbcTemplate.queryForRowSet(DBQueries.SELECT_LOG_DTLS_BY_ID, new Object[] { id });
+
+		LoginDetails logDtls = new LoginDetails();
+		while (rowSet.next()) {
+			logDtls.setId(rowSet.getInt("ID"));
+			logDtls.setName(rowSet.getString("NAME"));
+			logDtls.setLoginDate(rowSet.getDate("LOGIN_DTE"));
+			logDtls.setActualShift(rowSet.getString("ACTUAL_SHIFT"));
+			logDtls.setLoginTime(rowSet.getString("LOGIN_TIME"));
+			logDtls.setLogOffTime(rowSet.getString("LOGOFF_TIME"));
+			logDtls.setComments(rowSet.getString("COMMENTS"));
+		}
+
+		return logDtls;
 	}
 
 	public String updateById(LoginDetails logDtls) {
-		boolean flag = false;
-		for (LoginDetails login : logins) {
-			if (login.getId() == logDtls.getId()) {
 
-				login.setActualShift(logDtls.getActualShift());
-				login.setLoginTime(logDtls.getLoginTime());
-				login.setLogOffTime(logDtls.getLogOffTime());
-				login.setComments(logDtls.getComments());
-
-				flag = true;
-			}
-		}
-		return flag == true ? ("Updated record with id " + logDtls.getId()) : " Not updated ";
+		Object[] params = { logDtls.getActualShift(), logDtls.getLoginTime(), logDtls.getLogOffTime(),
+				logDtls.getComments(), logDtls.getId() };
+		int count = jdbcTemplate.update(DBQueries.UPD_LOG_DTLS_BY_ID, params);
+		return count > 0 ? ("Updated record with id " + logDtls.getId()) : " Not updated ";
 	}
-	
+
 	public String deleteById(Integer id) {
-		boolean flag = false;
-		Iterator<LoginDetails> itr = logins.iterator();
-		while(itr.hasNext()) {
-			LoginDetails logDtls = itr.next();
-			if(logDtls.getId()==id){
-				itr.remove();
-				flag=true;
-			}
-		}
-		
-		return flag == true ? ("Deleted record with id " + id ): " Not Deleted ";
-		
+		Object[] params = { id };
+		int count = this.jdbcTemplate.update(DBQueries.DEL_LOG_DTLS_BY_ID, params);
+		return count > 0 ? ("Deleted record with id " + id) : " Not Deleted ";
+
 	}
 
 }
